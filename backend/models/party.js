acfffb86948e0410d065976887fc1d87454cfc5f
Andre <andredelima92@gmail.com>
2023-04-25 22:17:20 -0300

    creating the models

---
+++ backend/models/party.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const PartySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  partyDate: {
+    type: Date,
+  },
+  photos: {
+    type: Array,
+  },
+  privacy: {
+    type: Boolean,
+  },
+  userId: {
+    type: mongoose.ObjectId,
+  },
+});
+
+const Party = mongoose.model("Party", PartySchema);
+
+module.exports = Party;
